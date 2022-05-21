89be9eb6efd0b12dfb20d26367adb144fb170a00
FalconRam <ramvignesh974@gamil.com>
2022-05-21 22:26:54 +0530

    Add User comp conneced with BE

---
+++ client/src/components/user/create-user.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const CreateUser = () => {
+  const [userName, setUserName] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const user = { userName };
+
+    axios
+      .post("http://localhost:5000/users/add", user)
+      .then((response) => console.log(response.data));
+    setUserName("");
+  };
+  return (
+    <div>
+      <h3>Create Your Profile</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>User Name :</label>
+          <input
+            type="text"
+            className="form-control"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input type="submit" className="btn btn-primary" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateUser;
